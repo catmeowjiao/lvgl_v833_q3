03d94c93acc211bff73a8ea6e31ce914156b89a3
meowjiao <meowjiao321@163.com>
2026-03-08 19:01:39 +0800

    尝试解决冲突

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
-#!/bin/bash
-
-make clean
-
-export TOOLCHAIN=/opt/musl-v833
+export TOOLCHAIN=$HOME/toolchain
 export PATH=$PATH:$TOOLCHAIN/bin
-export CC=$TOOLCHAIN/bin/arm-linux-musleabihf-gcc
-export CXX=$TOOLCHAIN/bin/arm-linux-musleabihf-g++
-export AR=$TOOLCHAIN/bin/arm-linux-musleabihf-ar
-export RANLIB=$TOOLCHAIN/bin/arm-linux-musleabihf-ranlib
-export STRIP=$TOOLCHAIN/bin/arm-linux-musleabihf-strip
+export CC=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-gcc
+export CXX=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-g++
+export AR=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ar
+export RANLIB=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-ranlib
+export STRIP=$TOOLCHAIN/bin/arm-openwrt-linux-muslgnueabi-strip
 
 export STAGING_DIR=$TOOLCHAIN/bin
 
